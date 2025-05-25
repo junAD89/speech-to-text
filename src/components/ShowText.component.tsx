d1de6c664983ee4d10df19968746dd7436561269
junAD89 <balhighlight@gmail.com>
2025-05-25 17:58:07 +0000

    new error fix

--- src/components/ShowText.component.tsx
+++ src/components/ShowText.component.tsx
@@ -8,7 +8,8 @@ export default function ShowTextcomponent() {
         transcript,
         listening,
         resetTranscript,
-        browserSupportsSpeechRecognition
+
+
     } = useSpeechRecognition();
 
     const startListening = () => SpeechRecognition.startListening({
